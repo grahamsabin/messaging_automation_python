0a66002d458958ff11a7ad0ec2720d011c056ed9
Graham Sabin <grahamcsabin@outlook.com>
2022-07-23 12:55:09 -0700

    updated env variables

--- app.py
+++ app.py
@@ -12,21 +12,28 @@ def index():
 
 
 def send_sms():
-    # account_sid = os.environ['AC9a514aeb37a22cc0629c8cfb4a47120f'] #def how it should be done
-    # auth_token = os.environ['bb3df30679555785dd9dad168f0e150d'] #this is how it should be done
-    account_sid = "AC9a514aeb37a22cc0629c8cfb4a47120f"
-    # Your Auth Token from twilio.com/console
-    auth_token = "2eeecc861a097a92cb8a2cb228f4472e"
+    # account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    # auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    sid_key = 'TWILIO_ACCOUNT_SID'
+    auth_key = 'TWILIO_AUTH_TOKEN'
+
+    account_sid = os.getenv(sid_key)
+    auth_token = os.getenv(auth_key)
+
+    print(account_sid, " this is sid")
+    print(auth_token, " this is auth")
+
     client = Client(account_sid, auth_token)
 
     message = client.messages \
         .create(
         body="Go to https://auto-messaging-python.herokuapp.com/runLogic you fool, you'll get a message",
         from_='+19854652607',
-        to='+12532234374'
+        to='+14255537653'
     )
     # return "<p>sms sent<p>"
-
+    # JOEY #: +12532234374
+    # GRAHAM #: +14255537653
 
 @app.route("/runLogic")
 def run_logic():
