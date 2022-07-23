6b54cddfd4561fba9b3b5a529063e48c9412050b
Graham Sabin <grahamcsabin@outlook.com>
2022-07-23 11:46:06 -0700

    changed logic flow

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from twilio.rest import Client
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     # base page, gives user instruction
     return "<p>Welcome to the general page, direct to {env}/sendSMS to send a message</p>"
 
 
-@app.route("/sendSMS")
-def sendSMS():
+def send_sms():
     # account_sid = os.environ['AC9a514aeb37a22cc0629c8cfb4a47120f'] #def how it should be done
     # auth_token = os.environ['bb3df30679555785dd9dad168f0e150d'] #this is how it should be done
     account_sid = "AC9a514aeb37a22cc0629c8cfb4a47120f"
     # Your Auth Token from twilio.com/console
-    auth_token = "bb3df30679555785dd9dad168f0e150d"
+    auth_token = "2eeecc861a097a92cb8a2cb228f4472e"
     client = Client(account_sid, auth_token)
 
     message = client.messages \
@@ -25,4 +25,10 @@ def sendSMS():
         from_='+19854652607',
         to='+14255537653'
     )
-    return "<p>sms sent<p>"
+    # return "<p>sms sent<p>"
+
+
+@app.route("/runLogic")
+def run_logic():
+    send_sms()
+    return "<p>logic check<p>"
