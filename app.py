0703a8ead3efdac7ade4041cc56b1c92a35e0d33
Graham Sabin <grahamcsabin@outlook.com>
2022-07-23 11:18:13 -0700

    Auth token updated

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def index():
 @app.route("/sendSMS")
 def sendSMS():
     # account_sid = os.environ['AC9a514aeb37a22cc0629c8cfb4a47120f'] #def how it should be done
-    # auth_token = os.environ['95f48287c0a7bdaeb9142958953aa05b'] #this is how it should be done
+    # auth_token = os.environ['bb3df30679555785dd9dad168f0e150d'] #this is how it should be done
     account_sid = "AC9a514aeb37a22cc0629c8cfb4a47120f"
     # Your Auth Token from twilio.com/console
-    auth_token = "95f48287c0a7bdaeb9142958953aa05b"
+    auth_token = "bb3df30679555785dd9dad168f0e150d"
     client = Client(account_sid, auth_token)
 
     message = client.messages \
